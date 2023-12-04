8165eb2fd1360e4e62837505b63c82383f2a0ee2
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2023-12-04 23:59:11 +0000

    cleanup

--- lib/fitfans/automation.rb
+++ lib/fitfans/automation.rb
@@ -17,22 +17,13 @@ module Fitfans
         # when "release beta"
         # when "release production"
         case arguments[0]
-        when "generate-network-layer" then generate_network_layer(arguments)
-        when "generate-icons" then generate_icons(arguments)
-        when "generate-assets" then generate_assets(arguments)
-        when "generate-splash-screen" then generate_splash_screen(arguments)
-        when "update-local-config" then update_local_config(arguments)
-        when "creators" then list_creators(arguments)
-        when "release" then release(arguments)
-        when "mass-release" then mass_release(arguments)
+        when "generate swagger" then generate_network_layer(arguments)
+        when "generate icons" then generate_icons(arguments)
+        when "generate assets" then generate_assets(arguments)
+        # when "generate-splash-screen" then generate_splash_screen(arguments)
+        when "release beta" then release(arguments)
         when "clean" then clean_rebuild(arguments)
-        when "co", "checkout", "change_to", "switch" then switch_to(arguments)
-        when "who" then who_am_i(arguments)
-        when "deploy" then deploy(arguments)
-        when "max" then Utils.highest_app_version
-        when "firebase" then Utils.configure_flutter
-        when "create" then create(arguments)
-        when "money" then money(arguments) # TODO: doesnt work yet
+        when "help" then help(arguments)
         else
 
           trainer, pattern_found = detect_pattern(arguments.join(' '), "swagger")
@@ -53,34 +44,6 @@ module Fitfans
         end
       end
 
-      def list_creators(arguments)
-        creators = Creators.get_creators(arguments[1])
-        max = Utils.highest_app_version(creators)
-        Creators.print_fancy_table(creators, max)
-      end
-
-      def create(arguments)
-        # Utils.interrupt_if_non_fitfans_project
-
-        if arguments.length != 3
-          puts "Required 2 parameters".red
-          exit
-        end
-
-        bundle_id = arguments[1]
-        app_name = arguments[2]
-
-        puts "gonna create #{bundle_id} #{app_name}"
-        # Creators.create_new_app(bundle)
-      end
-
-      def update_local_config(arguments)
-        Utils.title 'Updating config...'
-        Utils.update_local_config
-        config = Utils.read_config
-        puts JSON.pretty_generate(config)
-        puts "done."
-      end
 
       def detect_pattern(input, target)
         pattern = /(\w+) --open #{Regexp.escape(target)}/
@@ -104,47 +67,41 @@ module Fitfans
         end
 
         if arguments[1] == "beta"
-          Utils.interrupt_if_non_fitfans_project
           Utils.run_fastlane_beta
         else
           puts "Not ready for #{arguments[1].red} yet"
         end
       end
+      
+      def help(arguments)
+        # Define the rows of the table
+        rows = []
+        rows << ['clean'.green, 'Completely wipes all existing data from the system.']
+        rows << :separator
+        rows << ['release beta'.green, 'Initiates the process to release the current build to the beta track for testing.']
+        rows << :separator
+        rows << ['generate swagger'.green, 'Executes a function to generate a Swagger (OpenAPI) specification based on provided arguments.']
+        rows << :separator
+        rows << ['generate icons'.green, 'Runs the process to create and organize icon assets as per the specified arguments.']
+        rows << :separator
+        rows << ['generate assets'.green, 'Initiates asset generation and management based on the given arguments.']
 
-      # TODO: just fake data at the moment
-      def money(arguments)
-        bundle = arguments[1]
-        puts "=> #{"$843".green}/MRR | #{"$12,978".green} total revenue"
+
+        # Create a table
+        table = Terminal::Table.new :headings => ['Command', 'Description'], :rows => rows
+
+        # Print the table
+        puts table
       end
 
-      def who_am_i(arguments)
-        Utils.interrupt_if_non_fitfans_project
-        Creators.who_am_i
-      end
-
-      def mass_release(arguments)
-        # TODO: ONLY RELEASE if they're BEHIND the MAX?
-        active_creators = Creators.get_creators "active"
-        max = Utils.highest_app_version(active_creators)
-        Creators.print_fancy_table(active_creators, max)
-
-        active_creators.each { |x|
-          Creators.deploy_in_temporary_folder x["subdomain"]
-          Dir.chdir "/tmp/"
-          # sleep(5)
-        }
-      end
-
+      
       def generate_network_layer(arguments)
         Utils.interrupt_if_non_fitfans_project
         system("dart run build_runner build --delete-conflicting-outputs")
       end
 
       def generate_icons(arguments)
-        Utils.interrupt_if_non_fitfans_project
-        config = Utils.read_config
-        # puts config.logo_url
-        Utils.replace_launcher_icon(config['logo_url'])
+        # Utils.replace_launcher_icon(config['logo_url'])
       end
 
       def generate_assets(arguments)
@@ -170,12 +127,11 @@ module Fitfans
       #
       def clean_rebuild(arguments)
         # Utils.interrupt_if_non_fitfans_project
-
-        Utils.execute "bundle install"
+        # flutter clean; rm -rf ios/Podfile.lock; flutter pub get; cd ios; pod install; cd ..
         Utils.execute "flutter clean"
         Utils.execute "flutter pub get"
         Dir.chdir("./ios/") do
-          Utils.execute "bundle install"
+          # Utils.execute "bundle install"
           Utils.execute "rm -rf Podfile.lock"
           Utils.execute "bundle exec pod install"
         end
