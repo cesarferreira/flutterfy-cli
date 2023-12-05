63a27ccd135577f93e55f39b9d9019ef85c7095c
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2023-12-05 16:06:12 +0000

    fine tuned the help table

--- lib/f/automation.rb
+++ lib/f/automation.rb
@@ -20,8 +20,36 @@ module F
           when "bump" then clean_rebuild(arguments) # TODO: bump a build number
           when "help" then help(arguments)
           else
-            puts "Dont recognise the command"
+            
+            # puts arguments[0].isEmpty? ? "No command provided".red : "Dont recognise the command".red
+            # puts "Dont recognise the command"
+            arguments[0].nil? ? help(arguments) : puts("Dont recognise the command".red)
         end
+      end
+
+      # when "bump" then clean_rebuild(arguments) # TODO: bump a build number
+        
+      def help(arguments)
+        binary_name = "f"
+        # Define the rows of the table
+        rows = []
+        rows << ["#{binary_name} clean".green, 'Deep cleans the project and rebuilds it.']
+        rows << :separator
+        rows << ["#{binary_name} generate swagger".green, 'Executes a function to generate a Swagger (OpenAPI) client.']
+        rows << ["#{binary_name} generate icon".green, 'Generates the icons for the app.']
+        rows << ["#{binary_name} generate assets".green, 'Initiates asset generation and management. (using "flutter_launcher_icons")']
+        rows << :separator
+        rows << ["#{binary_name} open ios".green, 'Opens appstoreconnect website.']
+        rows << ["#{binary_name} open android".green, 'Opens play console website.']
+        rows << :separator
+        rows << ["#{binary_name} release beta".green, 'Initiates the process to release the current build to the beta track.']
+        rows << ["#{binary_name} release release".green, 'Initiates the process to release the current build to the production track.']
+        rows << :separator
+        rows << ["#{binary_name} help".green, 'Shows a table with all the available commands.']
+        # Create a table
+        table = Terminal::Table.new :headings => ['Command', 'Description'], :rows => rows
+
+        puts table
       end
 
       def handle_open_command(arguments)
@@ -34,6 +62,8 @@ module F
       end  
       
       def handle_generate_command(arguments)
+        Utils.interrupt_if_non_flutter_project
+
         case arguments[1]
           when "swagger" then generate_network_layer(arguments)
           when "icon" then generate_icons(arguments)
@@ -55,33 +85,13 @@ module F
         end
 
         case arguments[1]
-        when "beta" then Utils.run_fastlane_beta
+        when "beta" then Utils.run_fastlane('beta')
+        when "release" then Utils.run_fastlane('release')
         else
           puts "Not ready for #{arguments[1].red} yet"
         end
       end
-      
-      def help(arguments)
-        # Define the rows of the table
-        rows = []
-        rows << ['clean'.green, 'Completely wipes all existing data from the system.']
-        rows << :separator
-        rows << ['release beta'.green, 'Initiates the process to release the current build to the beta track for testing.']
-        rows << :separator
-        rows << ['generate swagger'.green, 'Executes a function to generate a Swagger (OpenAPI) specification based on provided arguments.']
-        rows << :separator
-        rows << ['generate icon'.green, 'Runs the process to create and organize icon assets as per the specified arguments.']
-        rows << :separator
-        rows << ['generate assets'.green, 'Initiates asset generation and management based on the given arguments.']
 
-        # Create a table
-        table = Terminal::Table.new :headings => ['Command', 'Description'], :rows => rows
-
-        # Print the table
-        puts table
-      end
-
-      
       def generate_network_layer(arguments)
         Utils.interrupt_if_non_flutter_project
         system("dart run build_runner build --delete-conflicting-outputs")
